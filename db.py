7f6be2f9819134221c08a328fd50b030bdcafa69
Anuj Verma <vanuj010@gmail.com>
2026-03-12 19:21:06 +0530

    fourth

--- db.py
+++ db.py
@@ -4,13 +4,14 @@ import os
 
 # ================= DATABASE CONNECTION =================
 
+
 def get_db():
     return mysql.connector.connect(
         host=os.environ.get("MYSQLHOST"),
         user=os.environ.get("MYSQLUSER"),
         password=os.environ.get("MYSQLPASSWORD"),
         database=os.environ.get("MYSQLDATABASE"),
-        port=int(os.environ.get("MYSQLPORT"))
+        port=int(os.environ.get("MYSQLPORT", 3306))
     )
 
 
